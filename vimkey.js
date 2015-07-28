d8b3127fef2f360cf4858f372b6b1607476e6a9a
闲耘™ <hotoo.cn@gmail.com>
2015-07-28 11:28:55 +0800

    refact

--- vimkey.js
+++ vimkey.js
@@ -1,9 +1,4 @@
-/**
- * @overview
- *
- * @author 闲耘™ (hotoo.cn[AT]gmail.com)
- * @version 2011/07/07
- */
+
 var KEY_CODE_MAP = {
   8  : ["<BS>"],              // Backspace
   9  : ["<Tab>"],
@@ -133,52 +128,11 @@ function hasMap(object, key) {
 }
 
 var E = {
-    KEY_BACKSPACE: 8,
-    KEY_TAB     : 9,
-    KEY_RETURN  : 13,
-    KEY_SHIFT   : 16,
-    KEY_CTRL    : 17,
-    KEY_CAPSLOCK: 20,
-    KEY_ESC     : 27,
-    KEY_LEFT    : 37,
-    KEY_UP      : 38,
-    KEY_RIGHT   : 39,
-    KEY_DOWN    : 40,
-    KEY_DELETE  : 46,
-    KEY_HOME    : 36,
-    KEY_END     : 35,
-    KEY_PAGEUP  : 33,
-    KEY_PAGEDOWN: 34,
-    KEY_INSERT  : 45,
-    KEY_0       : 48,
-    KEY_1       : 49,
-    KEY_2       : 50,
-    KEY_3       : 51,
-    KEY_4       : 52,
-    KEY_5       : 53,
-    KEY_6       : 54,
-    KEY_7       : 55,
-    KEY_8       : 56,
-    KEY_9       : 57,
-    KEY_WINDOWS : 91,
-    KEY_COMMA   : 188,
-    KEY_SEMICOLON: 186,
-    KEY_QUOTATION: 222,
-    KEY_SIGN    : 49,
     pause: function (evt){
         if (evt.stopPropagation) {
             evt.stopPropagation();
         } else {
             evt.cancelBubble = true;
-        }
-    },
-    stop : function(evt){
-        if(evt.stopPropagation){
-            evt.stopPropagation();
-            evt.preventDefault();
-        }else{
-            evt.cancelBubble = true;
-            evt.returnValue = false;
         }
     },
     add : function(elem, evt, handler){
